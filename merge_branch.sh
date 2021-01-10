7f59d7d1ddc3aecd97da8103ffb6947121f9b01f
Sam Sepiol <0.alicegawa@gmail.com>
2021-01-10 13:23:07 +0900

    shellscript for merging branches

---
+++ merge_branch.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+set -eu
+BRANCHNAME="$(git rev-parse --abbrev-ref HEAD)"
+IFS='/' read -r -a array <<< "$BRANCHNAME"
+BRANCHTYPE="${array[0]}"
+CURRENTVERSION="${array[1]}"
+echo $BRANCHTYPE
+echo $CURRENTVERSION
+
+
+get_merge_branches() {
+    case $BRANCHTYPE in
+        feature)
+            echo "development"
+            ;;
+        hotfix)
+            echo "master development"
+            ;;
+        release)
+            echo "master development"
+    esac
+}
+branches=`get_merge_branches`
+IFS=' ' read -r -a array <<< "$branches"
+for i in "${array[@]}"
+do
+    "$(git checkout $i)"
+    "$(git merge --no-ffcheckout $BRANCHNAME)"
+    if [ $i = 'master' ]; then
+        "$(git tag -a '$CURRENTVERSION')"
+    fi
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
