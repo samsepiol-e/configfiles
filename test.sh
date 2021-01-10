e752cf2d9b53180f113224b9a4cf4b62c8db0e97
Sam Sepiol <0.alicegawa@gmail.com>
2021-01-10 09:11:27 +0900

    test

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+THEME=powerlevel10k/powerlevel10k
+PLUGINS=""
+ZSHRC_APPEND=""
+
+while getopts ":t:p:a:" opt; do
+    case ${opt} in
+        t)  THEME=$OPTARG
+            ;;
+        p)  PLUGINS="${PLUGINS}$OPTARG "
+            ;;
+        a)  ZSHRC_APPEND="$ZSHRC_APPEND\n$OPTARG"
+            ;;
+        \?)
+            echo "Invalid option: $OPTARG" 1>&2
+            ;;
+        :)
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+echo $1
