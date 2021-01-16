03ead2db0c122791358c13ca9fae53e2c0c2055b
Sam Sepiol <0.alicegawa@gmail.com>
2021-01-16 13:22:00 +0900

    uncomment

--- version.sh
+++ version.sh
@@ -22,7 +22,7 @@ if [[ "$1" == "feature" ]]; then
   fi
   newbranchname="$1/$2"
 elif [[ "$1" == "hotfix" ]]; then
-  build=$(echo $build + 1 | bc)
+  build=$(echo $build+1 | bc)
   newbranchname="$1/${major}.${minor}.${build}"
 elif [[ "$1" == "release" ]]; then
   minor=$(echo $minor+1 | bc)
@@ -47,11 +47,11 @@ fi
 echo "old version : $version"
 echo "new version : ${major}.${minor}.${build}"
 echo $newbranchname
-#echo "${major}.${minor}.${build}">currentversion
-##adding current version
-#git add currentversion
-#git checkout -b $newbranchname  $branch_origin
-#git push -u origin $newbranchname
-#git checkout -b $1/${major}.${minor}.${build} $branch_origin
-##push to remote branch
-##git push -u origin $1/${major}.${minor}.${build}
+echo "${major}.${minor}.${build}">currentversion
+#adding current version
+git add currentversion
+git checkout -b $newbranchname  $branch_origin
+git push -u origin $newbranchname
+git checkout -b $1/${major}.${minor}.${build} $branch_origin
+#push to remote branch
+#git push -u origin $1/${major}.${minor}.${build}
